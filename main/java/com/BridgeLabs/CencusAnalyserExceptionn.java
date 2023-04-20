8e7ba8c8d80ca51d0f99e610ec094ac85e7ee6a2
anuroopkanna <anuroopindia16@gmail.com>
2023-04-20 05:38:40 +0530

    first commit

---
+++ main/java/com/BridgeLabs/CencusAnalyserExceptionn.java
@@ -0,0 +1,19 @@
+package com.BridgeLabs;
+
+public class CencusAnalyserExceptionn extends Exception{
+    enum ExceptionType {
+        CENSUS_FILE_PROBLEM
+    }
+
+    ExceptionType type;
+
+    public CensusAnalyserException(String message, ExceptionType type) {
+        super(message);
+        this.type = type;
+    }
+
+    public CensusAnalyserException(String message, ExceptionType type, Throwable cause) {
+        super(message, cause);
+        this.type = type;
+    }
+}
---
+++ test/java/CencusAnalyserTest.java
@@ -0,0 +1,38 @@
+import com.BridgeLabs.CencusAnalyser;
+
+public class CencusAnalyserTest {
+    private static final String INDIA_CENSUS_CSV_FILE_PATH = "C:\\Users\\anuro\\IdeaProjects\\Day29cencus\\src\\main\\java\\com\\BridgeLabs";
+    private static final String WRONG_CSV_FILE_PATH = "./src/main/resources/IndiaStateCensusData.csv";
+
+    @Test
+    public void givenIndianCensusCSVFileReturnsCorrectRecords() {
+        try {
+            CencusAnalyser censusAnalyser = new CencusAnalyser();
+            int numOfRecords = censusAnalyser.loadIndiaCensusData(INDIA_CENSUS_CSV_FILE_PATH);
+            Assert.assertEquals(29,numOfRecords);
+        } catch (cencusAnalayserException e) { }
+    }
+
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM,e.type);
+        }
+    }
+    @Test
+    public void givenIndiaCensusData_WithWrongFile_ShouldThrowException() {
+        try {
+            CensusAnalyser censusAnalyser = new CensusAnalyser();
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(CensusAnalyserException.class);
+            censusAnalyser.loadIndiaCensusData(WRONG_CSV_FILE_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.CENSUS_FILE_PROBLEM, e.type);
+        }
+    }
+}
